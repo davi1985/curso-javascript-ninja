abf38813ac3707d3809c5f78b3374b933c40e562
Davi Silva <davisilvaphoto@gmail.com>
2019-01-17 15:42:22 -0300

    Exercicio concluido

--- challenge-21/challenge-21.js
+++ challenge-21/challenge-21.js
@@ -1,17 +1,39 @@
-/*
-O desafio de hoje será um pequeno projeto: um cronômetro!
-As regras para criação do cronômetro são as seguintes:
-1. Crie um arquivo index.html e adicione esse script a ele;
-2. Crie um campo `input` do tipo `text`, e inicie-o com um valor 0 (zero).
-Ele será o nosso cronômetro;
-3. Crie 3 botões para as ações do cronômetro: Start, Stop e Reset;
-4. Ao clicar em Start, o valor do campo deve ser incrementado de 1 em 1, a
-cada segundo;
-5. Ao clicar em Stop, o cronômetro deve parar de contar;
-6. Ao clicar em Reset, o cronômetro deve zerar e parar de contar.
+(function(window,document) {
+    'use strict';
+    /*
+    O desafio de hoje será um pequeno projeto: um cronômetro!
+    As regras para criação do cronômetro são as seguintes:
+    1. Crie um arquivo index.html e adicione esse script a ele;
+    2. Crie um campo `input` do tipo `text`, e inicie-o com um valor 0 (zero).
+    Ele será o nosso cronômetro;
+    3. Crie 3 botões para as ações do cronômetro: Start, Stop e Reset;
+    4. Ao clicar em Start, o valor do campo deve ser incrementado de 1 em 1, a
+    cada segundo;
+    5. Ao clicar em Stop, o cronômetro deve parar de contar;
+    6. Ao clicar em Reset, o cronômetro deve zerar e parar de contar.
 
-Utilize o atributo data-js para nomear o campo e os botões. Você pode
-usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
-dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
-*/
-// ?
+    Utilize o atributo data-js para nomear o campo e os botões. Você pode
+    usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
+    dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
+    */
+    let $cronos = document.querySelector('[data-js="cronos"]');
+    let timer;
+    const $btnStart = document.querySelector('[data-js="btn-start"]');
+    $btnStart.addEventListener('click', function() {
+        timer = setInterval(() => {
+            $cronos.value++;
+        }, 1000);
+    });
+
+    const $btnStop = document.querySelector('[data-js="btn-stop"]');
+    $btnStop.addEventListener('click', () => {
+        clearInterval(timer);
+    })
+
+    const $btnReset = document.querySelector('[data-js="btn-reset"]');
+    $btnReset.addEventListener('click', () => {
+        $cronos.value = 0;
+    });
+
+
+})(window,document);
--- exemplos/js/main.js
+++ exemplos/js/main.js
@@ -40,17 +40,79 @@
 
 
     //.value
-    var $inputUsername = doc.querySelector('#username');
-    var $inputPassword = doc.querySelector('#username');
-    var $button = doc.querySelector('#button');
+    // var $inputUsername = doc.querySelector('#username');
+    // var $inputPassword = doc.querySelector('#username');
+    // var $button = doc.querySelector('#button');
 
-    $button.addEventListener('click', function(event) {
-        event.preventDefault();
-        console.log('Click no botão');
-    }, false);
+    // $button.addEventListener('click', function(event) {
+    //     event.preventDefault();
+    //     console.log('Click no botão');
+    // }, false);
 
-    $inputUsername.addEventListener('click', function(event) {
-        alert('Clicou');
+    // $inputUsername.addEventListener('click', function(event) {
+    //     alert('Clicou');
+    // },false);
+
+    //sync vs async
+
+    // console.log('inicio');
+    // document.addEventListener('click', function() {
+    //     console.log('clicou no documento');
+    // })
+    // console.log('fim');
+    
+    //setTimeout()
+
+    // console.log('inicio');
+    // setTimeout(function() {
+    //     console.log('executou setTimeout');
+    // }, 1000);
+    // console.log('fim');   
+
+    //setInterval()
+
+    // console.log('inicio');
+    // setInterval(function() {
+    //     console.log('executou setInterval');
+    // }, 1000);
+    // console.log('fim'); 
+
+
+    //função recursiva
+
+    // var counter = 0;
+    // function timer() {
+    //     console.log('timer', counter++);
+    //     if(counter > 10)
+    //         return;
+    //     setTimeout(timer, 1000);
+    // }
+    // timer();
+
+    var counter = 0;
+    var $button = document.querySelector('[data-js="button"]');
+    var temporizador;
+    //parando o setTimeout
+    // function timer() {
+    //     console.log('timer', counter++);
+    //     if(counter > 20)
+    //         return;
+    //     temporizador = setTimeout(timer, 500);
+    // }
+
+    // $button.addEventListener('click', function() {
+    //     clearTimeout(temporizador);
+    // },false);
+    // timer();
+
+    //parando o setInterval()
+    function timer() {
+        console.log('timer', counter++);
+    }
+    temporizador = setInterval(timer, 500);
+
+    $button.addEventListener('click', function() {
+        clearInterval(temporizador);
     },false);
-
+        
 })(window, document);
